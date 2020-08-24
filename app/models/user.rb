a5b8c51caad5a3bc013989c6eefbc381d197a838
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-08-24 15:46:34 +0100

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates :name, presence: true,uniqueness: true, length: { minimum: 2 ,message: "Name must be present"}
+    validates :post, length: {in: 5..1000 }, presence: true
 end
