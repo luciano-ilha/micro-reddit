a8cddb0ab2fd8b8812ebe3df65cf639830b624d4
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-08-24 16:36:34 +0100

    Add validation to Post class

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :title, length: { minimum: 2 }, presence: true
+  validates :body, length: { in: 5..1000 }, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :posts
     validates :name, presence: true, length: { minimum: 2 ,message: "Name must be present"}
     validates :password, length: {in: 5..50 }, presence: true
 end
