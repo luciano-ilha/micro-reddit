6ca2451456ceede8492b8f23f8ecf6f561005346
Luciano Ilha Carbonell <ilha.dev.br@gmail.com>
2020-08-25 05:55:29 -0300

    Add user id to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :post
+  belongs_to :user
+  validates :coms, length: { in: 5..1000 }, presence:true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_153958) do
+ActiveRecord::Schema.define(version: 2020_08_25_084956) do
 
   create_table "comments", force: :cascade do |t|
     t.text "coms"
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
